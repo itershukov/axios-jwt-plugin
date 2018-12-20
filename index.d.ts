61d921e7d948fc9d7afdb09cd8ebb6c1cab430b2
Ivan Tershukov <plopr@ya.ru>
2018-12-20 10:42:36 +0300

    add d.ts

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+import { IAsyncStorage } from 'universal-storage';
+import { AxiosStatic } from 'axios';
+interface IConfig {
+    storage: IAsyncStorage;
+    axios: AxiosStatic;
+    refreshTokenURL?: string;
+}
+export declare function configureAxiosJWTInterseptors(config: IConfig): void;
+export declare function saveCreds(creds: any): Promise<string | void>;
+export declare function clearCreds(): Promise<string | void>;
+export {};
