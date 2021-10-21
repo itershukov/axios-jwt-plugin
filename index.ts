1ccf9d1c41e855ec36500f9af78ceb45b0da53e1
Ivan Tershukov <plopr@ya.ru>
2021-10-21 21:03:39 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,14 +1,29 @@
 import { IAsyncStorage } from 'universal-storage';
-import { AxiosInstance, AxiosStatic } from 'axios';
-
-const defaultRefreshTokenURL = '/api/refresh-token';
+import { AxiosInstance, AxiosResponse, AxiosStatic } from 'axios';
 
 let tokenUpdater: Promise<any> | null = null;
 
 interface IConfig {
   storage: IAsyncStorage;
   axios: AxiosStatic;
-  refreshTokenURL?: string;
+  refreshTokenEndpoint: string;
+  convertToCamelCase?: boolean;
+  getCredsFromRefreshResponse?: (res: AxiosResponse) => ICreds;
+  onSaveCreds?(creds: any): void | Promise<void>;
+  onGetCreds?(creds: any): void | Promise<void>;
+  onClearCreds?(): void | Promise<void>;
+}
+
+export interface ICreds {
+  access?: Token;
+  refresh?: Token;
+}
+export interface Token {
+  id: string;
+  token: string;
+  userId: string;
+  issuedAt: number;
+  expiredAt: number;
 }
 
 enum tokenStatuses {
@@ -19,12 +34,14 @@ enum tokenStatuses {
   badSignature = 14
 }
 
-const PRE_REFRESH_PERIOD = 10;
+const preRefreshPeriod = 10;
 
 let refreshInstance: AxiosInstance;
 
 let storage: IAsyncStorage;
 let axios: AxiosStatic;
+let convertToCamelCase: boolean;
+let globalConfig: IConfig;
 
 export function configureAxiosJWTInterseptors(config: IConfig) {
   if (storage && axios) {
@@ -33,14 +50,16 @@ export function configureAxiosJWTInterseptors(config: IConfig) {
 
   storage = config.storage;
   axios = config.axios;
-  const refreshTokenURL = config.refreshTokenURL || defaultRefreshTokenURL;
+  convertToCamelCase = config.convertToCamelCase === undefined ? true : !!config.convertToCamelCase;
+  globalConfig = config;
+
   refreshInstance = axios.create({
-    timeout: 1000
+    timeout: (preRefreshPeriod / 2) * 1000
   });
 
   axios.interceptors.request.use(
     async conf => {
-      await _refreshTokenIfNeeded(refreshTokenURL);
+      await refreshTokenIfNeeded(config);
       if (axios.defaults.headers.common['Authorization']) {
         conf.headers['Authorization'] = axios.defaults.headers.common['Authorization'];
       }
@@ -70,7 +89,7 @@ export function configureAxiosJWTInterseptors(config: IConfig) {
       }
 
       try {
-        await _refreshToken(refreshTokenURL);
+        await refreshToken(config);
         return axios(originalRequest);
       } catch (e) {
         console.error(e);
@@ -80,57 +99,65 @@ export function configureAxiosJWTInterseptors(config: IConfig) {
   );
 }
 
-async function _refreshTokenIfNeeded(refreshTokenURL: string) {
+async function refreshTokenIfNeeded(config: IConfig) {
   const { access, refresh } = await getCreds();
 
   if (!access || !refresh) {
     return;
   }
 
+  const refreshCamel = camelCase(refresh);
+  const accessCamel = camelCase(access);
+
+  if (!axios.defaults.headers.common['Authorization'] && access.token) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${access.token}`;
+  }
+
   try {
-    const now = Date.now() / 1000;
+    const now = Math.round(Date.now() / 1000);
     switch (true) {
-      case !!access.token:
-        axios.defaults.headers.common['Authorization'] = `Bearer ${access.token}`;
+      case refreshCamel.expiredAt < now:
         break;
-      case refresh.expired_at < now:
-        break;
-      case access.expired_at < now:
-        await _refreshToken(refreshTokenURL);
-        break;
-      case access.expired_at < now + PRE_REFRESH_PERIOD: // if token expired soon
-        _refreshToken(refreshTokenURL);
+      case accessCamel.expiredAt < now:
+      case accessCamel.expiredAt < now + preRefreshPeriod: // if token expired soon
+        await refreshToken(config);
         break;
       default:
         break;
     }
   } catch (e) {
-    console.warn('_refreshTokenIfNeeded');
+    console.warn('refreshTokenIfNeeded');
   }
 }
 
-async function _refreshToken(refreshTokenURL: string) {
+export async function refreshToken(config: IConfig) {
   const { refresh } = await getCreds();
   if (!refresh) {
     throw Error();
   }
 
   if (!tokenUpdater) {
+    const refreshTokenKey = convertToCamelCase ? 'refreshToken' : 'refresh_token';
+    delete refreshInstance.defaults.headers.common.Authorization;
+
     tokenUpdater = refreshInstance
-      .post(
-        refreshTokenURL,
+      .put(
+        config.refreshTokenEndpoint,
         {
-          refresh_token: refresh.token
+          [refreshTokenKey]: refresh.token
         },
         {
           baseURL: axios.defaults.baseURL
         }
       )
       .then(async res => {
-        return await saveCreds(res.data.data);
+        const creds = _getCredsFromRes(res, config);
+        return await saveCreds(creds);
       })
       .catch(async e => {
-        await clearCreds();
+        if (e && e.status === 401) {
+          await clearCreds();
+        }
         throw e;
       })
       .finally(() => {
@@ -141,27 +168,70 @@ async function _refreshToken(refreshTokenURL: string) {
   return tokenUpdater;
 }
 
-export async function saveCreds(creds: any) {
+export async function saveCreds(creds: ICreds) {
   if (!creds.access || !creds.access.token) {
     return;
   }
 
   axios.defaults.headers.common['Authorization'] = `Bearer ${creds.access.token}`;
-  return await storage.setItem('creds', JSON.stringify(creds));
+  const preparedCreds = convertToCamelCase ? camelCase(creds) : creds;
+  globalConfig && globalConfig.onSaveCreds && globalConfig.onSaveCreds(preparedCreds);
+  return await storage.setItem('creds', JSON.stringify(preparedCreds));
 }
 
 export async function clearCreds() {
-  delete axios.defaults.headers.common['Authorization'];
-  return await storage.setItem('creds', '');
+  try {
+    delete axios.defaults.headers.common['Authorization'];
+    globalConfig && globalConfig.onClearCreds && globalConfig.onClearCreds();
+    return await storage.setItem('creds', '');
+  } catch (e) {
+    console.warn('Error at clearCreds method!', e);
+    return;
+  }
 }
 
 export async function getCreds() {
   try {
-    const credsItem = (await storage.getItem('creds')) || '{}';
+    const credsItem = (storage && (await storage.getItem('creds'))) || '{}';
     const creds = JSON.parse(credsItem);
+
+    globalConfig && globalConfig.onGetCreds && globalConfig.onGetCreds(creds);
     return creds;
   } catch (e) {
-    console.warn('getCreds', e);
+    console.warn('Error at getCreds method!', e);
     return {};
   }
+}
+
+function camelCase(obj: { [k: string]: any }) {
+  let newObj: { [k: string]: any } = {};
+  for (const d in obj) {
+    if (obj.hasOwnProperty(d)) {
+      const newKey = d.replace(/(\_\w)/g, function(m) {
+        return m[1].toUpperCase();
+      });
+      newObj[newKey] = obj[d];
+    }
+  }
+  return newObj;
+}
+
+function _getCredsFromRes(res, config): ICreds {
+  if (config.getCredsFromRefreshResponse) {
+    const result = config.getCredsFromRefreshResponse(res);
+    if (result.access) {
+      return result;
+    }
+    throw new Error('Function getCredsFromRefreshResponse wrong implemented. It should return data compatible with ICreds.');
+  }
+
+  if (res.data.access) {
+    return res.data;
+  }
+
+  if (res.data.data.access) {
+    return res.data.data;
+  }
+
+  throw new Error("Can't parse response to get tokens");
 }
